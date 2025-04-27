aab1709ae912666cb781f2e6d21e6dff45b1cff2
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2025-04-27 15:57:05 +0200

    Create Tkinter window

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# Import module to generate QR-code
 from ensurepip import version
 from pickle import TRUE
 from turtle import fillcolor
 import qrcode
 from qrcode.constants import ERROR_CORRECT_L
+
+# Import Tkinter for GUI
+from tkinter import *
 
 qr = qrcode.QRCode(
     version=1,
@@ -14,3 +18,12 @@ qr.add_data('https://www.google.com/')
 qr.make(fit=TRUE)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save('google.png')
+
+fenetre = Tk()
+champ_label = Label(fenetre, text="Generate QR-code", bg="#000033", fg="white")
+champ_label.pack()
+fenetre.configure(bg="#000033")
+canvas=Canvas(fenetre, width=800, height=600)
+canvas.pack()
+
+fenetre.mainloop()
